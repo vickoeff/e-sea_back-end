49f0966cffd0b15a3a4bd9e1934192cba6c2a8fd
mananjabrik <manan.jabrik@gmail.com>
2023-10-24 04:35:21 +0700

    remove unused and setup crud user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,22 +10,30 @@ export class UsersService {
     return await this.prisma.user.create({
       data: createUserDto,
     });
-    // return 'hey';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return await this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    return await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return await this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    return await this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
