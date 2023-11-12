ec8eefa7afedb4643e540b17ec7e523d1ca1a0b9
mananjabrik <manan.jabrik@gmail.com>
2023-11-12 10:18:23 +0700

    feat: update detinantion path uploaded file

--- src/announcement/announcement.module.ts
+++ src/announcement/announcement.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AnnouncementService } from './services/announcement.service';
 import { AnnouncementController } from './controller/announcement.controller';
 import { PrismaService } from 'src/repository/prisma.service';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      dest: './upload',
+    }),
+  ],
   controllers: [AnnouncementController],
   providers: [AnnouncementService, PrismaService],
 })
